79efba6d616fc0dd53811390af544b7c97de2edb
Bantu Janardhan <janardhanbantu9@gmail.com>
2026-07-18 12:09:24 +0530

    added new pictures for ui

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,1 +1,32 @@
+var users = [
+    {
+        "name" : "dhurandhar",
+        "gender" : "Male",
+        "img" : "anidhu.jpeg"
+    },
+    {
+        "name" : "wat da dawg doin",
+        "gender" : "dawg",
+        "img" : "nerd.jpeg"
+    }
+]
 
+var id = 0;
+
+function toggleUser(){
+    id = (id + 1)%2
+    // get the elements
+
+    // 1: image
+    var userImage = document.getElementById("user-image");
+    userImage.src = users[id].img;
+
+    // 2: name
+    var userName = document.getElementById("user-name");
+    userName.innerHTML = users[id].name;
+
+    // 3: gender
+    var userGender = document.getElementById("user-gender");
+    userGender.innerHTML = "Gender: " + users[id].gender;
+
+}
